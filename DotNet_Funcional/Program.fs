9c8f59f7658f0a8de5f462b1ba861e2e8a7e1930
RichardKT88 <kendy_t88@hotmail.com>
2021-09-20 14:13:49 -0300

    Add project files.

---
+++ DotNet_Funcional/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+let olaMundo() = 
+    printfn "Olá mundo!" 
+
+let elevaNumeroAoQuadrado numero = numero * numero
+
+let SomaQuadradoDosNumerosAteDez = 
+    [1..10] |> List.map elevaNumeroAoQuadrado |> List.sum
+
+[<EntryPoint>]
+let main argv =
+    olaMundo()
+    0 
